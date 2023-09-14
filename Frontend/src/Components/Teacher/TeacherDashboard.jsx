5dd7227066c04902e52db54c7e159054c6d93931
Viruli <viruliweerasinghe@gmail.com>
2023-09-14 17:12:56 +0530

    change dashboard

--- Frontend/src/Components/Teacher/TeacherDashboard.jsx
+++ Frontend/src/Components/Teacher/TeacherDashboard.jsx
@@ -75,7 +75,7 @@ function TeacherDashboard(props) {
                   <span className="icon">
                     <ion-icon name="cog-outline"></ion-icon>
                   </span>
-                  <span className="title1">Settings</span>
+                  <span className="title1">Reset Password</span>
                 </a>
               </li>
 
